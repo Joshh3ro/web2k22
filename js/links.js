b0252a8fbfbbb7cfb4955539b6b31d09379d409c
Joshua D.C <35842209+Joshh3ro@users.noreply.github.com>
2022-05-13 15:33:46 +0200

    Addition of pages and routing
    Simple routing
    more complex routing will be handled in react or next js when i get to that point

--- js/links.js
+++ js/links.js
@@ -1,3 +1,17 @@
+//Internal Links
+function Home() {window.open("../index.html", "_top"); }
+
+//Portfolio Links
+function Dreams() {window.open("../index.html", "_top"); }
+function Panda() {window.open("../index.html", "_top"); }
+function Poly() {window.open("../index.html", "_top"); }
+function XDPortfolio() {window.open("../index.html", "_top"); }
+function Shape() {window.open("../index.html", "_top"); }
+function PortfolioMore() {window.open("./Views/portfolio.html", "_top"); }
+//Information Links
+function AboutMe() {window.open("../index.html", "_top"); }
+function Credits() {window.open("../index.html", "_top"); }
+// External Links
 function Twitter() {window.open("https://twitter.com/Josh_HeroTV", "_blank"); }
 function Behance() {window.open("https://www.behance.net/Josh_Hero", "_blank"); }
 function Instagram() {window.open("https://www.instagram.com/josh_h3ro/?hl=en", "_blank"); }
